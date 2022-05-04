7e88dce5caf34ee872dcde28dc6179eb1ce0c27b
nelsojo <nelsojo4@oregonstate.edu>
2022-05-04 12:01:20 -0700

    practice
    changes

--- wordBoxes.js
+++ wordBoxes.js
@@ -2,11 +2,18 @@ var allWords = [];
 var currentWord = 0;
 
 function handleNewWordsEntered(event) {
-  var text = '';
+  console.log("==handleNewWordsEnter() Called")
+  var text = event.currentTarget.value
+  console.log("--Text entered: ", text)
   allWords = text.replace(/[!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~]/g, '')
     .toLowerCase().split(' ');
+    console.log("--allWords: ", allWords)
   currentWord = 0;
 }
+
+var wordsInput = document.getElementById('words-input') 
+//wordsInput.addEventListener('input', handleNewWordsEntered) for assign 3
+wordsInput.addEventListener('change', handleNewWordsEntered)
 
 function generateWordElem(word, highlightColor) {
 	var wordElem = document.createElement('span');
@@ -18,3 +25,35 @@ function generateWordElem(word, highlightColor) {
   }
   return wordElem;
 }
+
+function computeHighlight() {
+var select = document.getElementById('every-nth-select')
+var userSelection = parsInt(select.value)
+if ((currentWord+1)%userSelection===0){
+
+  var radioButtons = document.querySelector('input[name="highlight-color"]:checked')
+  console.log("==radioButtons: ", radioButtons)
+  return radioButtons.value
+} else {
+  return false
+}
+}
+
+function handleButtonClick (event){
+console.log('==A button was clicked')
+var word = allWords[currentWord]
+console.log("==Word: ", word)
+if (word){
+  var wordElem = generateWordElem(word, 'blue')
+  console.log("==wordElem: ", wordElem)
+
+var container = event.currentTarget.parentNode.parentNode
+var wordsContainer = container.querySelector('.words-container')
+wordsContainer.appendChild(wordElem)
+  currentWord = (currentWord+1) % allWords.length
+}
+}
+
+var buttons = document.getElementsByClassName('add-word-button')
+ for (i=0; i<buttons.length; i++)
+ buttons[i].addEventListener('click', handleButtonClick)
